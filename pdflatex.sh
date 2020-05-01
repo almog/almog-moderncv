d93d81038ea211aff02009a1e0760d9631ccb5cb
Almog Kurtser <akurtser@gmail.com>
2020-05-01 12:34:04 +0300

    Nest packages in a subdir

--- pdflatex.sh
+++ pdflatex.sh
@@ -1,7 +1,11 @@
 #!/bin/sh
+
+export TEXINPUTS="./packages:"
+
 if [ -x "$(command -v pdflatex)" ]; then
   pdflatex "$@"
 else
 	docker image inspect "blang/latex:latest"  >/dev/null 2>&1 || docker pull "blang/latex"
-  ./dockercmd pdflatex "$@"
+  ./dockercmd pdflatex -output-directory=logs "$@" 
+  mv logs/*.pdf ./
 fi
